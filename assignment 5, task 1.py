451253214192f4221ac4a7a4cb1354e08ca0c63f
ZeeshuAD <zeeshanahmed9060600@gmail.com>
2025-07-29 08:35:47 +0530

    Add files via upload

---
+++ assignment 5, task 1.py
@@ -0,0 +1,12 @@
+student_name = input('Enter the student\'s name: ')
+a= {'Alice:85', 'Mary: 95', 'John: 91', 'Rocky: 83'}
+if student_name== 'Alice':
+    print("Alice's marks: 85")
+elif student_name== 'Mary':
+    print("Mary's marks: 95")
+elif student_name=='John':
+    print("John's marks: 91")
+elif student_name=='Rocky':
+    print("Rocky's marks: 83")
+else:
+    print('Student not found.')
---
+++ assignment 5, task 2.py
@@ -0,0 +1,5 @@
+list= [1,2,3,4,5,6,7,8,9,10]
+a= list[0:5]
+print('Extracted first five elements: ',a)
+a.reverse()
+print('Reversed extracted elements: ', a)
